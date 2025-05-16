940d4c21d14c10fbe8a0223489b7fac371315000
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-05-16 06:38:23 +0530

    Time: 6 ms (3.30%) | Memory: 9.7 MB (5.36%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,14 +3,16 @@ public:
     string mergeAlternately(string word1, string word2) {
         string result = "";
         int i  = 0;
-        while(i<word1.length() || i<word2.length()){
+        int j = 0;
+        while(i<word1.length() || j<word2.length()){
             if(i<word1.length()){
                 result = result+word1[i];
             }
-            if(i<word2.length()){
-                result = result+word2[i];
+            if(j<word2.length()){
+                result = result+word2[j];
             }
             i++;
+            j++;
         }
         return result;
     }
