49e1bda805aa7d12eb548a47eb08ed419d6c9e18
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-05-21 09:57:29 +0530

    Time: 7 ms (23.69%) | Memory: 24.1 MB (24.25%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -0,0 +1,53 @@
+class NumArray {
+private:
+    vector<int> seg;
+    int n;
+    void build(int ind, int low, int high, vector<int>& nums) {
+        if (low == high) {
+            seg[ind] = nums[low];
+            return;
+        }
+        int mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, nums);
+        build(2 * ind + 2, mid + 1, high, nums);
+        seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+    }
+
+    void update(int ind, int low, int high, int i, int val) {
+        if (low == high) {
+            seg[ind] = val;
+            return;
+        }
+        int mid = (low + high) / 2;
+        if (i <= mid)
+            update(2 * ind + 1, low, mid, i, val);
+        else
+            update(2 * ind + 2, mid + 1, high, i, val);
+        seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+    }
+
+    int query(int ind, int low, int high, int l, int r) {
+        if (r < low || high < l) return 0; // no overlap
+        if (l <= low && high <= r) return seg[ind]; // total overlap
+        int mid = (low + high) / 2;
+        int left = query(2 * ind + 1, low, mid, l, r);
+        int right = query(2 * ind + 2, mid + 1, high, l, r);
+        return left + right;
+    }
+public:
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        seg.resize(4*n);
+        build(0, 0, n - 1, nums);
+    }
+    
+    int sumRange(int left, int right) {
+        return query(0, 0, n - 1, left, right);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
