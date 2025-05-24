8efd5d30666356a24af51f70e21965967f36d17e
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-05-24 22:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (21.76%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int leftpro = 1;
+        int rightpro = 1;
+        int ans = nums[0];
+        for(int i = 0 ; i<nums.size() ; i++){
+            leftpro = leftpro==0?1 : leftpro;
+            rightpro = rightpro==0?1 : rightpro;
+        
+        leftpro *= nums[i];
+        rightpro *= nums[nums.size()-1-i];
+
+        ans = max(ans , max(leftpro , rightpro));
+        }
+        return ans ; 
+
+    }
+};
