69dde984589ed1c9a5bcbdf425ad4781a970c9e4
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-06-17 10:59:00 +0530

    Time: 37 ms (25.86%) | Memory: 27.1 MB (46.21%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(string &s,string &t,int m,int n){
-        if(n==0) return dp[m][n]=1;
-        if(m==0) return dp[m][n]=0;
+    int solve(string &s, string &t, int m, int n, vector<vector<int>> &dp){
+        if(n == 0) return dp[m][n] = 1;
+        if(m == 0) return dp[m][n] = 0;
 
-        if(dp[m][n]!=-1) return dp[m][n];
+        if(dp[m][n] != -1) return dp[m][n];
 
-        if(s[m-1]==t[n-1]){
-            return dp[m][n]=solve(s,t,m-1,n)+solve(s,t,m-1,n-1);
+        if(s[m-1] == t[n-1]){
+            return dp[m][n] = solve(s, t, m-1, n, dp) + solve(s, t, m-1, n-1, dp);
         }
-        return dp[m][n]=solve(s,t,m-1,n);
+        return dp[m][n] = solve(s, t, m-1, n, dp);
     }
-    int numDistinct(string s, string t) {
-        int m=s.length(),n=t.length();
 
-        memset(dp,-1,sizeof(dp));
-        return solve(s,t,m,n);
+    int numDistinct(string s, string t) {
+        int m = s.length(), n = t.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));  
+        return solve(s, t, m, n, dp);
     }
 };
