b8fbf92d5be5e0c60d48e373bf34cbfe5fd3cc0f
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-06-26 15:08:31 +0530

    Time: 50 ms (27.46%) | Memory: 49.3 MB (29.08%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        if (amount < 1) return 0;
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i >= coin && dp[i - coin] != INT_MAX) {
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
-                }
+    int solve(int i, int tar, vector<int>& coins, vector<vector<int>>& dp) {
+        if (i == 0) {
+            if (tar % coins[0] == 0) return tar / coins[0];
+            return INT_MAX;
+        }
+        if (dp[i][tar] != -1) return dp[i][tar];
+
+        int ntake = solve(i - 1, tar, coins, dp);
+        int take = INT_MAX;
+        if (coins[i] <= tar) {
+            int res = solve(i, tar - coins[i], coins, dp);
+            if (res != INT_MAX) {
+                take = 1 + res;
             }
         }
-        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+
+        return dp[i][tar] = min(ntake, take);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int ans = solve(n - 1, amount, coins, dp);
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
