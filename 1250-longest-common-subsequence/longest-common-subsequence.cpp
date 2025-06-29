a38212eba6d8d3dc30211cf6a4d76b2c1f503fbe
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-06-29 18:32:21 +0530

    Time: 44 ms (16.09%) | Memory: 27.5 MB (28.06%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
+int f(int idx1 , int idx2 , string &s1 ,string &s2 , vector<vector<int>>&dp){
+    if(idx1 < 0 || idx2 < 0){
+        return 0;
+    }
+    if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+    if(s1[idx1] == s2[idx2]){
+        return dp[idx1][idx2] = 1 + f(idx1 -1 , idx2 - 1 , s1 , s2 , dp);
+    }
+    return dp[idx1][idx2] = max(f(idx1 - 1 , idx2 , s1 , s2 , dp) , f(idx1 , idx2 - 1 , s1 , s2 , dp));
+}
     int longestCommonSubsequence(string text1, string text2) {
-      int n = text1.size() + 1;
-      int m = text2.size() + 1;
-      vector<vector<int>> grid(n, vector<int>(m, 0));
-
-      for(int i = 1 ; i < n ; i++){
-        for(int j = 1 ; j < m ; j++){
-            if(text1[i-1] == text2[j-1]){
-                grid[i][j] = 1 + grid[i-1][j-1];
-            }else{
-                grid[i][j] = max(grid[i-1][j] , grid[i][j-1]);
-            }
-        }
-      }
-      return grid[n-1][m-1];  
+      int n = text1.size();
+      int m = text2.size();
+      vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+      return f(n-1 , m-1 ,text1 , text2, dp); 
     }
 };
