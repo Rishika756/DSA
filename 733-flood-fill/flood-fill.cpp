1f7e019df2cb4be8eb7351e46441f613e2f128dd
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-08-06 11:16:39 +0530

    Time: 4 ms (6.05%) | Memory: 18.2 MB (60.46%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>>& ans, vector<vector<int>>& image, 
-             int newcolor, int initialcolor, vector<int>& delrow, vector<int>& delcol) {
-        ans[row][col] = newcolor;
-        int n = image.size();
-        int m = image[0].size();
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-                image[nrow][ncol] == initialcolor && ans[nrow][ncol] != newcolor) {
-                dfs(nrow, ncol, ans, image, newcolor, initialcolor, delrow, delcol);
-            }
+void dfs(vector<vector<int>>&ans , vector<vector<int>>& image , int row , int col , int &color , int &icolor , vector<int>&drow, vector<int>&dcol){
+    ans[row][col] = color;
+    int n = image.size();
+    int m = image[0].size();
+    for(int i = 0 ; i<4 ; i++){
+        int nr = row + drow[i];
+        int nc = col + dcol[i];
+        if(nr >=0 && nr<n && nc >= 0 && nc < m && image[nr][nc] == icolor && ans[nr][nc] != color){
+            dfs(ans, image, nr, nc, color, icolor, drow, dcol);
         }
     }
-
+}
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int inicolor = image[sr][sc];
-        vector<vector<int>> ans = image;
-        vector<int> delrow = {-1, 0, 1, 0};
-        vector<int> delcol = {0, 1, 0, -1};
-        dfs(sr, sc, ans, image, color, inicolor, delrow, delcol);
+        int icolor = image[sr][sc];
+        vector<vector<int>>ans = image;
+        vector<int>drow = {-1 , 0 , 1 , 0};
+        vector<int>dcol = {0 , 1 , 0 , -1};
+        dfs(ans , image , sr , sc , color , icolor , drow , dcol);
         return ans;
     }
 };
