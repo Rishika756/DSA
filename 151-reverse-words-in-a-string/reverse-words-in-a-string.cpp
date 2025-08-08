84cca213cbcdeaf4fc8c0859014c096eb5025205
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-08-08 10:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (68.22%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,44 +1,21 @@
 class Solution {
 public:
     string reverseWords(string s) {
-    //     int n = s.size();
-    //     string ans = "";
-
-    //     int i = 0;
-    //     while (i < n && s[i] == ' ') i++; 
-    //     int j = n - 1;
-    //     while (j >= 0 && s[j] == ' ') j--; 
-
-    //     string temp = "";
-    //     for (; i <= j; i++) {
-    //         if (s[i] != ' ') {
-    //             temp += s[i];
-    //         } else if (!temp.empty()) {
-    //             ans = temp + (ans.empty() ? "" : " ") + ans;
-    //             temp = "";
-    //         }
-    //     }
-
-    //     if (!temp.empty()) {
-    //         ans = temp + (ans.empty() ? "" : " ") + ans;
-    //     }
-
-    //     return ans;
-    // }
-    int n = s.size();
-    string ans = "";
-    reverse(s.begin() , s.end());
-    for(int i = 0 ; i<=n ; i++){
-        string word = "";
-        while(i<n && s[i]!=' '){
-            word+= s[i];
-            i++;
+        int n = s.size();
+        string ans = "";
+        reverse(s.begin() , s.end());
+        for(int  i = 0; i<=n ; i++){
+            string word ="";
+            while(s[i]!= ' ' && i<n){
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin() , word.end());
+            if(word.size()>0){
+                ans += " " + word;
+            }
         }
-        reverse(word.begin() , word.end());
-        if(word.size()>0){
-            ans += " " + word;
-        }
-    }
-    return ans.substr(1);
+        return ans.substr(1);
+
     }
 };
