c6b403da50734096cc7dfadd99c5739f86343daa
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-08-16 23:16:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.40%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-         int n = nums.size();
-        int idx1 = -1, idx2 = -1;  // initialize to -1 in case not found
-        int i = 0, j = n - 1;
-
-        while (i <= j) {
-            if (nums[i] == target && idx1 == -1) {
-                idx1 = i;
-            } else if (idx1 == -1) {
-                i++;
+    vector<int> searchRange(vector<int>& arr, int t) {
+        int n = arr.size();
+        vector<int>ans(2 , -1);
+        int low = 0 ; 
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(arr[mid]>=t){
+                high = mid -1;
+            }else{
+                low = mid+1;
             }
-
-            if (nums[j] == target && idx2 == -1) {
-                idx2 = j;
-            } else if (idx2 == -1) {
-                j--;
-            }
-
-            if (idx1 != -1 && idx2 != -1) {
-                break;
+            if(arr[mid] == t){
+                ans[0] = mid;
             }
         }
+        low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] <= t) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            if (arr[mid] == t) ans[1] = mid;
+        }
+        return ans;
 
-        return {idx1, idx2};
     }
 };
