99ed0ddc2d70da9ed5b72022e4265bc5170dd156
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-11-10 21:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (8.89%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,18 +4,15 @@ public:
         int low = 0;
         int high = nums.size()-1;
         while(low<=high){
-            int mid = low + (high - low) / 2;
-
-            if(nums[mid] == target){
+            int mid = low +(high-low)/2;
+            if(nums[mid]==target){
                 return mid;
-            }
-            else if(target > nums[mid]){
-                low = mid + 1;
-            }
-            else{
-                high = mid -1;
+            }else if(target>nums[mid]){
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
         }
-        return -1;;
+        return -1;
     }
 };
