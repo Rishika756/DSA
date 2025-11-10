5d5e31f95c372057a305b3b264cf46fc35df26b3
Rishika756 <159421553+Rishika756@users.noreply.github.com>
2025-11-10 21:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (97.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // int isp = 0;
+        // for(int i = 0 ; i<nums.size() ; i++){
+        //     if(nums[i]!=0){
+        //         nums[isp] = nums[i];
+        //         isp++;
+        //     }
+        // }
+        // while(isp < nums.size()){
+        //     nums[isp++] = 0;
+        // }
         int isp = 0;
-        for(int i = 0 ; i<nums.size(); i++){
+        int n = nums.size();
+        for(int i = 0 ; i<n ; i++){
             if(nums[i]!=0){
                 nums[isp] = nums[i];
                 isp++;
             }
         }
-        while(isp<nums.size()){
-            nums[isp] = 0;
-            isp++;
+        while(isp<n){
+            nums[isp++] = 0;
         }
     }
 };
